16db0b9909e49d643b41dce2281230639b269f68
Silva Daniel, SUB-SDC-NL-MBM-BNC <Daniel.Silva@swisscom.com>
2023-11-06 15:48:21 +0100

    muda versao do hashicorp

--- versions.tf
+++ versions.tf
@@ -2,25 +2,34 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.15.0"
     }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+    
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.1"
+    }
   }
+
+  backend "remote" {
+		hostname = "app.terraform.io"
+		organization = "lanchonetebairro"
+
+		workspaces {
+			name = "lanchonete-db"
+		}
+	}
 }
 
 provider "aws" {
   region = "us-east-1"
-}
-
-terraform {
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "lanchonetebairro"
-
-    workspaces {
-      name = "lanchonete-db"
-    }
-  }
 }
